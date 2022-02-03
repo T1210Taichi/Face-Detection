2f58fb07d9fd01cd5d43457ef513f91a57fc2ca2
T1210Taichi <T1210Taichi@gmail.com>
2022-02-03 18:28:41 +0900

    顔の大きさに合わせてキャラクターをリサイズ

--- setCharacter.py
+++ setCharacter.py
@@ -8,8 +8,8 @@ cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #キャラクター
 character = cv2.imread("character.png")
-character = cv2.resize(character,dsize=(250,250))
-chr_height, chr_width = character.shape[:2]
+#character = cv2.resize(character,dsize=(250,250))
+#chr_height, chr_width = character.shape[:2]
 
 # VideoCapture オブジェクトを取得します
 capture = cv2.VideoCapture(0)
@@ -19,14 +19,18 @@ while(True):
     frame_height,frame_width = frame.shape[:2]
 
     #顔の学習データ精査
-    front_face_list=cascade.detectMultiScale(frame,scaleFactor=1.1,minNeighbors=3,minSize=(90,90))
-    #print(front_face_list)
+    front_face_list=cascade.detectMultiScale(frame,scaleFactor=1.1,minNeighbors=3,minSize=(100,100))
+    print(front_face_list)
 
     #画像を当てはめる
-    for (x,y,w,h) in front_face_list:
-        if x + chr_width < frame_width and y + chr_height < frame_height:
-            frame[y:y+chr_height,x:x+chr_width] = character
-            cv2.imshow('frame',frame)  
+    if len(front_face_list)>0:
+        for (x,y,w,h) in front_face_list:
+            #顔の大きさに合わせて画像をリサイズ
+            character_re = cv2.resize(character,dsize=(w,h))
+            chr_height, chr_width = character_re.shape[:2]
+            if x + chr_width < frame_width and y + chr_height < frame_height:
+                frame[y:y+chr_height,x:x+chr_width] = character_re
+                cv2.imshow('frame',frame)  
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
